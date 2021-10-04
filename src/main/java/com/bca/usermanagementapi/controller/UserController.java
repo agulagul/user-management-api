1d3890656925000e6e5617249b32197ade7a578b
BCADOMAIN\U069359 <gusti_saputra@bca.co.id>
2021-10-05 06:48:20 +0700

    Penambahan error handling

--- src/main/java/com/bca/usermanagementapi/controller/UserController.java
+++ src/main/java/com/bca/usermanagementapi/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.bca.usermanagementapi.controller;
 
-import java.io.ObjectInputFilter.Status;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,80 +35,134 @@ public class UserController {
     @PostMapping
     public ResponseEntity<Response<User>> createUser(@Valid @RequestBody User user, Errors errors){
         Response<User> response = new Response<>();
-        
-        if(errors.hasErrors()){
-            for (ObjectError error : errors.getAllErrors()) {
-                response.getMessages().add(error.getDefaultMessage());
+        try {
+            
+            if(errors.hasErrors()){
+                for (ObjectError error : errors.getAllErrors()) {
+                    response.getMessages().add(error.getDefaultMessage());
+                }
+                response.setStatus(StatusCode.ERROR);
+                response.setPayload(null);
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
             }
-            response.setStatus(StatusCode.ERROR);
-            response.setPayload(null);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-        }
+    
+            response.setStatus(StatusCode.OK);
+            response.getMessages().add("User added succesfully");
+            response.setPayload(userService.saveUser(user));
+            return ResponseEntity.status(HttpStatus.OK).body(response);
 
-        response.setStatus(StatusCode.OK);
-        response.getMessages().add("User added succesfully");
-        response.setPayload(userService.saveUser(user));
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            response.setStatus(StatusCode.ERROR);
+            response.getMessages().add(e.getMessage());
+            response.setPayload(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
     
     @GetMapping
     public ResponseEntity<Response<List<User>>> getUsers(){
         Response<List<User>> response = new Response<>();
-        List<User> users = new ArrayList<>();
-        userService.getUsers().forEach((user)->users.add(user));
-        response.setPayload(users);
-        if(users.isEmpty()){
-            response.setStatus(StatusCode.NOT_FOUND);
-            response.getMessages().add("There is no user");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+
+        try {
+            
+            List<User> users = new ArrayList<>();
+            userService.getUsers().forEach((user)->users.add(user));
+            response.setPayload(users);
+            if(users.isEmpty()){
+                response.setStatus(StatusCode.NOT_FOUND);
+                response.getMessages().add("There is no user");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+            }
+            
+            response.setStatus(StatusCode.OK);
+            response.getMessages().add("Found user(s)");
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+            
+        } catch (Exception e) {
+            response.setStatus(StatusCode.ERROR);
+            response.getMessages().add(e.getMessage());
+            response.setPayload(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
-        
-        response.setStatus(StatusCode.OK);
-        response.getMessages().add("Found user(s)");
-        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Response<User>> getUserById(@PathVariable("id") Long id){
         Response<User> response = new Response<>();
-        User user = userService.getUserById(id);
-        if(user == null){
-            response.setStatus(StatusCode.NOT_FOUND);
-            response.getMessages().add("There is no user");
+
+        try {
+            
+            User user = userService.getUserById(id);
+            if(user == null){
+                response.setStatus(StatusCode.NOT_FOUND);
+                response.getMessages().add("There is no user");
+                response.setPayload(null);
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+            }
+            
+            response.setStatus(StatusCode.OK);
+            response.getMessages().add("Found user(s)");
+            response.setPayload(user);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+            
+        } catch (Exception e) {
+
+            response.setStatus(StatusCode.ERROR);
+            response.getMessages().add(e.getMessage());
             response.setPayload(null);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+
         }
-        
-        response.setStatus(StatusCode.OK);
-        response.getMessages().add("Found user(s)");
-        response.setPayload(user);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PutMapping
     public ResponseEntity<Response<User>> updateUser(@Valid @RequestBody User user, Errors errors){
         Response<User> response = new Response<>();
-        if(errors.hasErrors()){
-            for (ObjectError error : errors.getAllErrors()) {
-                response.getMessages().add(error.getDefaultMessage());
-            }
-            response.setStatus(StatusCode.ERROR);
-            response.setPayload(null);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-        }
 
-        response.setStatus(StatusCode.OK);
-        response.setPayload(userService.saveUser(user));
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        try {
+            
+            if(errors.hasErrors()){
+                for (ObjectError error : errors.getAllErrors()) {
+                    response.getMessages().add(error.getDefaultMessage());
+                }
+                response.setStatus(StatusCode.ERROR);
+                response.setPayload(null);
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+            }
+    
+            response.setStatus(StatusCode.OK);
+            response.getMessages().add("Update success");
+            response.setPayload(userService.saveUser(user));
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+            
+        } catch (Exception e) {
+
+            response.setStatus(StatusCode.ERROR);
+            response.getMessages().add(e.getMessage());
+            response.setPayload(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Response<Long>> deleteUserById(@PathVariable("id") Long id){
         Response<Long> response = new Response<>();
-        userService.deleteUserById(id);
-        response.setStatus(StatusCode.OK);
-        response.getMessages().add("User deleted");
-        response.setPayload(id);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        try {
+            
+            userService.deleteUserById(id);
+            response.setStatus(StatusCode.OK);
+            response.getMessages().add("User deleted");
+            response.setPayload(id);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+            
+        } catch (Exception e) {
+            
+            response.setStatus(StatusCode.ERROR);
+            response.getMessages().add(e.getMessage());
+            response.setPayload(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+
+        }
     }
 }
